fed244e2fa4ed9448700cef76dacb10e035c28fd
Atirek Gupta <agupta@thoughtworks.com>
2014-05-16 08:39:38 +0530

    [Atirek] implemented Channing

---
+++ src/main/java/flow/BookingFlow.java
@@ -0,0 +1,11 @@
+package flow;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: atirek
+ * Date: 16/05/14
+ * Time: 8:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookingFlow {
+}
